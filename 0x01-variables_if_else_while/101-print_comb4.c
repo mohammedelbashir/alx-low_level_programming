ee2d925faf21d9c04babd04e5474c940b82d4261
mohammedelbashir <mohammedelbashiralihammad@gmail.com>
2023-10-08 08:19:51 -0700

    FIX ADVANCE

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,3 @@
-/*
- * File: 10-print_comb2.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
@@ -17,14 +12,14 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = digit1 +1; digit2 < 10; digit2++)
 		{
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 			putchar((digit3 % 10) + '0');
-			if (digit1 == 9 && digit2 == 9)
+			if (digit1 == 8 && digit2 == 9)
 				continue;
 			if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 				break;
