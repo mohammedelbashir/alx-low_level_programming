824246d526de0eaa0df6e511d96e9e79bcc4c1e1
mohammedelbashir <mohammedelbashiralihammad@gmail.com>
2023-10-23 18:37:11 -0700

    donee

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,7 @@
+/*
+ * File: 1-memcpy.c
+ */
+
 #include "main.h"
 
 /**
@@ -15,4 +19,4 @@ char *_memcpy(char *dest, char *src, unsigned int n)
         for (i = 0; i < n; i++)
                 dest[i] = src[i];
         return (dest);
-}
+)
